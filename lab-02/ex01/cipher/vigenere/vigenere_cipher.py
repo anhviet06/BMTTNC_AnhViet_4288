1d2eec1c001be8dcaa35cc0b83bb250852f6e156
buianhviet06 <buianhviet06@gmail.com>
2026-03-05 21:40:30 +0700

    ban gop chung

--- lab-02/ex01/cipher/vigenere/vigenere_cipher.py
+++ lab-02/ex01/cipher/vigenere/vigenere_cipher.py
@@ -1,20 +1,17 @@
 class VigenereCipher:
     def __init__(self):
         pass
-    
+
     def vigenere_encrypt(self, plain_text, key):
         encrypted_text = ""
         key_index = 0
         for char in plain_text:
             if char.isalpha():
-                # Xác định độ dời từ ký tự hiện tại của key
                 key_shift = ord(key[key_index % len(key)].upper()) - ord('A')
-                
                 if char.isupper():
                     encrypted_text += chr((ord(char) - ord('A') + key_shift) % 26 + ord('A'))
                 else:
                     encrypted_text += chr((ord(char) - ord('a') + key_shift) % 26 + ord('a'))
-                
                 key_index += 1
             else:
                 encrypted_text += char
@@ -25,14 +22,11 @@ class VigenereCipher:
         key_index = 0
         for char in encrypted_text:
             if char.isalpha():
-                # Xác định độ dời ngược lại từ ký tự của key
                 key_shift = ord(key[key_index % len(key)].upper()) - ord('A')
-                
                 if char.isupper():
                     decrypted_text += chr((ord(char) - ord('A') - key_shift) % 26 + ord('A'))
                 else:
                     decrypted_text += chr((ord(char) - ord('a') - key_shift) % 26 + ord('a'))
-                
                 key_index += 1
             else:
                 decrypted_text += char
